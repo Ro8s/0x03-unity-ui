f0dea94a31f4a05806258aeabd93074476217b4c
Ro8s <rodrigo.delgado3132@gmail.com>
2022-01-04 16:58:18 -0300

    added ex 12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void PlayMaze()
-    {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+    private Color colorTrap;
+    private Color colorGoal;
+    
+   public void PlayMaze()
+   {
         SceneManager.LoadScene("maze");
+   }
+
+    private void Update()
+    {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else if (colorblindMode.isOn == false)
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
     }
+
+
+
     public void QuitMaze()
     {
         Debug.Log("Quit Game");
